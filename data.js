11c04cdeab2d74db8d379365d0fc7a6d2d67e60f
saltysaff <d_ash12345@hotmail.co.uk>
2023-03-13 17:26:14 +0000

    started implementing grid

---
+++ data.js
@@ -0,0 +1,44 @@
+articles = [
+    {
+        title: 'Blog one',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-1.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        title: 'Blog two',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-2.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        title: 'Blog three',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-3.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    },
+    {
+        title: 'Blog four',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-4.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    }
+    {
+        title: 'Blog five',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-5.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    }
+    {
+        title: 'Blog six',
+        date: 'July 23, 2022',
+        image: 'images/blog-img-6.jpg',
+        blurb: `I'm excited to start a new learning journey as a Scrimba Bootcamp student!
+                After several months of learning in the Frontend Developer Career Path.`
+    }
+]
